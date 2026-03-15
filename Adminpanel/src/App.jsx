88f2f23cd3dc3d82e06ddbea63d5a1a9ca3a2bac
01Kiruthika <kiruthikasundaram2004@gmail.com>
2026-03-15 19:40:13 +0530

    Admin panel Using Router

--- Adminpanel/src/App.jsx
+++ Adminpanel/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-import './App.css'
+import './index.css'
 import Header from './components/Header'
 import Aside from './components/Aside'
 import Main from './components/Main'
--- Adminpanel/src/components/Main.jsx
+++ Adminpanel/src/components/Main.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Dashboard from '../Pages/Dashboard'
+import Product from '../Pages/Product'
+import Category from '../Pages/Category'
+import Customer from '../Pages/Customer'
 
 const Main = () => {
     return (
         <main>
-            <h2>main</h2>
+            <Routes>
+                <Route path='/' element={<Dashboard />}></Route>
+                <Route path='/dashboard' element={<Dashboard />}></Route>
+                <Route path='/product' element={<Product />}></Route>
+                <Route path='/category' element={<Category />}></Route>
+                <Route path='/customer' element={<Customer />}></Route>
+            </Routes>
         </main>
     )
 }
