72e88d483c23f05ca650208d593103b6ba8e480c
01Kiruthika <kiruthikasundaram2004@gmail.com>
2026-03-20 21:37:13 +0530

    task complete

--- useEffect-Task/src/App.jsx
+++ useEffect-Task/src/App.jsx
@@ -2,35 +2,15 @@ import { useEffect, useState } from 'react'
 
 import './App.css'
 import axios from 'axios'
+import VoiceInput from './VoiceInput'
 
 function App() {
-  const [count, setCount] = useState([])
-
-  useEffect(() => {
-    let loaddata = async () => {
-      let apidata = await axios.get('https://freegoldapi.com/data/latest.json')
-      // console.log(apidata.data)
-      setCount(apidata.date)
-    }
-    loaddata()
-    console.log(count);
-
-  }, [])
+ 
 
   return (
     <>
-      <div className="container">
-        <div className='box'>
-          <h3>Gold Tracking System</h3>
-          <input type="date"
-            placeholder='Enter the Date'
-          /><br></br>
-          <button className='btn'>Submit</button>
-
-          {/* <p>0</p> */}
-        </div>
-
-
+      <div>
+        <VoiceInput />
       </div>
     </>
   )
---
+++ useEffect-Task/src/VoiceInput.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+const VoiceInput = () => {
+    const [text, setText] = useState("");
+    const [listening, setListening] = useState(false);
+
+    let recognition;
+
+    if ("webkitSpeechRecognition" in window) {
+        recognition = new window.webkitSpeechRecognition();
+        recognition.continuous = false;
+        recognition.interimResults = false;
+        recognition.lang = "en-IN"; 
+
+        recognition.onresult = (event) => {
+            const transcript = event.results[0][0].transcript;
+            setText(transcript);
+            setListening(false);
+        };
+
+        recognition.onerror = (event) => {
+            console.error(event.error);
+            setListening(false);
+        };
+    } else {
+        alert("Your browser does not support Speech Recognition");
+    }
+
+    const startListening = () => {
+        setListening(true);
+        recognition.start();
+    };
+
+    const stopListening = () => {
+        recognition.stop();
+        setListening(false);
+    };
+
+    return (
+        <div className="box">
+            <h2> Voice to Text (React)</h2>
+            <button onClick={startListening} disabled={listening}>
+                Start Listening
+            </button>
+            <button onClick={stopListening} disabled={!listening} style={{ marginLeft: "10px" }}>
+                Stop
+            </button>
+            <h3>Output:</h3>
+            <p>{text}</p>
+        </div>
+    );
+};
+
+export default VoiceInput;
